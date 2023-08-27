5c51acc650325861b1440197c1122b434d7fab50
Austin817 <jack830930@yahoo.com.tw>
2023-08-27 19:54:08 +0800

    分別開發與產品路徑

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // 開發中 or 產品路徑
+  base: process.env.NODE_ENV === 'production' ? '/React/' : '/',
   plugins: [react()],
 })
